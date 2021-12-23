a909023a6b308ae14736d74827bc74ab66a7dde5
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 17:40:09 -0300

    feat: add quant to CheckStock and remove cart ref

--- src/domain/interactors/add-product.js
+++ src/domain/interactors/add-product.js
@@ -1,28 +1,26 @@
-const CheckProductAvailableStock = require('../check-product-available-stock')
+const CheckProductAvailableStock = require('./check-product-available-stock')
 
 class AddProduct {
-  constructor({ cartRepository, productRepository }) {
-    this.cartRepository = cartRepository
+  constructor({ productRepository }) {
     this.productRepository = productRepository
   }
 
   async execute({ cartId, product }) {
+    const { sku, quantity } = product
+
     const checkProductAvailableStock = new CheckProductAvailableStock()
 
     const productAvailable = await checkProductAvailableStock.execute({
-      sku: product.sku,
+      sku,
+      quantity,
     })
 
     if (!productAvailable.inStock) throw new Error('Product Unavailable')
 
     await this.productRepository.create({
+      cart_id: cartId,
       ...product,
-      cartId,
     })
-
-    const cart = await this.cartRepository.getById({ id: cartId })
-
-    return cart
   }
 }
 
