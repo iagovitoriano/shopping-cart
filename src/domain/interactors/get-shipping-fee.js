5154f22fb79122c4cfb63cd09ba985de8c60924a
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 19:13:27 -0300

    feat: simulate external call to shipping service

---
+++ src/domain/interactors/get-shipping-fee.js
@@ -0,0 +1,13 @@
+const { sleep } = require('../helpers')
+
+class GetShippingFee {
+  async execute() {
+    await sleep(1500)
+
+    return {
+      fee: parseFloat((Math.random() * (22 - 5) + 5).toFixed(2)),
+    }
+  }
+}
+
+module.exports = GetShippingFee
