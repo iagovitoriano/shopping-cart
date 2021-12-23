e96bce8d77adb9f9a5667aa2825e4a835c0a2e19
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 13:54:37 -0300

    feat: parse json, set cors and security headers

---
+++ src/transportLayer/http/middlewares/index.js
@@ -0,0 +1,9 @@
+const securityHeaders = require('./security-headers')
+const bodyParser = require('./body-parser')
+const cors = require('./cors')
+
+module.exports = {
+  securityHeaders,
+  bodyParser,
+  cors,
+}
