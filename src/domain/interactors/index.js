b9f3b6d41f8debb84dec5c5ecd0506aab5870572
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 20:07:06 -0300

    style: change interactors orders to module.exports

--- src/domain/interactors/index.js
+++ src/domain/interactors/index.js
@@ -1,15 +1,15 @@
 const CreateCart = require('./create-cart')
 const GetCart = require('./get-cart')
+const ClearCart = require('./clear-cart')
 const AddProduct = require('./add-product')
 const UpdateProductQuantity = require('./update-product-quantity')
 const RemoveProduct = require('./remove-product')
-const ClearCart = require('./clear-cart')
 
 module.exports = {
   CreateCart,
   GetCart,
+  ClearCart,
   AddProduct,
   UpdateProductQuantity,
   RemoveProduct,
-  ClearCart,
 }
