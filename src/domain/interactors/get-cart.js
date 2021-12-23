b4a78c35451b3e875a880079f3dcb606b80bcc10
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 19:00:56 -0300

    feat: add product list to cart

--- src/domain/interactors/get-cart.js
+++ src/domain/interactors/get-cart.js
@@ -1,13 +1,21 @@
 class GetCart {
-  constructor({ cartRepository }) {
+  constructor({ cartRepository, productRepository }) {
     this.cartRepository = cartRepository
+    this.productRepository = productRepository
   }
 
   async execute({ id }) {
     const cart = await this.cartRepository.findById({ id })
+    const products = await this.productRepository.findMany({
+      conditions: {
+        cart_id: id,
+        deleted: false,
+      },
+    })
 
     return {
       ...cart,
+      products,
     }
   }
 }
