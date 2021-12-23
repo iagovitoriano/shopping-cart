e19edef694cb6fcdeea92a79fb9dceeaee365bd5
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 17:47:17 -0300

    feat: centralize interactors exports

---
+++ src/domain/interactors/index.js
@@ -0,0 +1,13 @@
+const CreateCart = require('./create-cart')
+const GetCart = require('./get-cart')
+const AddProduct = require('./add-product')
+const UpdateProductQuantity = require('./update-product-quantity')
+const RemoveProduct = require('./remove-product')
+
+module.exports = {
+  CreateCart,
+  GetCart,
+  AddProduct,
+  UpdateProductQuantity,
+  RemoveProduct,
+}
