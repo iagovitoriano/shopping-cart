39bb13acd2e84ebc217c62b302618fcf11d25a2f
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-24 00:09:38 -0300

    build: adjust mongodb uri on env variables

--- src/transportLayer/http/config/env.js
+++ src/transportLayer/http/config/env.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 module.exports = {
   port: process.env.PORT || 4033,
-  mongoUri: process.env.MONGO_URL || 'mongodb://localhost:27017',
+  mongoUri: process.env.MONGO_URL || 'mongodb://db_mongo:27017',
   dbName: process.env.DB_NAME || 'development',
 }
