b60eb7adc1274d77ad1857cd08db35b3710a57dc
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 23:43:57 -0300

    feat: add product management routes

--- src/transportLayer/http/routes/cart-routes.js
+++ src/transportLayer/http/routes/cart-routes.js
@@ -5,14 +5,26 @@ const {
   getCartController,
   clearCartController,
   addDiscountCoupon,
+  addProductController,
+  removeProductController,
+  updateProductQuantityController,
 } = require('../controllers')
 
 module.exports = (router) => {
-  router.post('/cart/', expressRouterAdapter(createCartController))
-  router.get('/cart/:id', expressRouterAdapter(getCartController))
-  router.patch('/cart/:id/clear', expressRouterAdapter(clearCartController))
-  router.patch(
-    '/cart/:id/discount-coupon',
-    expressRouterAdapter(addDiscountCoupon)
-  )
+  router
+    .post('/cart/', expressRouterAdapter(createCartController))
+    .get('/cart/:id', expressRouterAdapter(getCartController))
+    .patch('/cart/:id/clear', expressRouterAdapter(clearCartController))
+    .patch('/cart/:id/discount-coupon', expressRouterAdapter(addDiscountCoupon))
+
+  router
+    .post('/cart/:cartId/product', expressRouterAdapter(addProductController))
+    .patch(
+      '/cart/:cartId/product/:id/quantity',
+      expressRouterAdapter(updateProductQuantityController)
+    )
+    .patch(
+      '/cart/:cartId/product/:id/clear',
+      expressRouterAdapter(removeProductController)
+    )
 }
