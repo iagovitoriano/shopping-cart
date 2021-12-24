9b47241fa2ae57a2c5ef2bb35e122a46dca6d538
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 22:49:37 -0300

    feat: implement GetCart controller

---
+++ src/transportLayer/http/controllers/get-cart-controller.js
@@ -0,0 +1,22 @@
+const status = require('http-status')
+
+const { getCartCompose } = require('../composers')
+
+class GetCartController {
+  async route(httpRequest) {
+    const { id } = httpRequest.params
+
+    const getCartInteractor = await getCartCompose()
+
+    const cart = await getCartInteractor.execute({
+      id,
+    })
+
+    return {
+      status: status.OK,
+      body: cart,
+    }
+  }
+}
+
+module.exports = new GetCartController()
