537cb2c825bd22925a180b278c1deb2334097554
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 22:28:58 -0300

    refactor: move app.require to execute after DB up

--- src/transportLayer/http/server.js
+++ src/transportLayer/http/server.js
@@ -1,12 +1,13 @@
 const MongoDB = require('../../dataSources/mongodb')
 const { port, mongoUri, dbName } = require('./config/env')
-const app = require('./config/app')
 
 MongoDB.connect({
   uri: mongoUri,
   dbName,
 })
   .then(() => {
+    const app = require('./config/app')
+
     app.listen(port, () => console.log(`Server running at PORT ${port}`))
   })
   .catch(console.error)
