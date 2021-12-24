88eb4d24e898db64c26ccccf8b7d20ffc6b465d0
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 22:51:07 -0300

    feat: use routes in express.Router

--- src/transportLayer/http/config/setup-router.js
+++ src/transportLayer/http/config/setup-router.js
@@ -1,7 +1,11 @@
 const { Router } = require('express')
 
+const { cartRoutes } = require('../routes')
+
 module.exports = (app) => {
   const router = Router()
 
-  app.use(router)
+  app.use('/api/v1', router)
+
+  cartRoutes(router)
 }
