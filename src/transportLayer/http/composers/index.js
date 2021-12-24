23c4026812f42861bc74e2bfe571d6d286afa779
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 22:43:18 -0300

    feat: centralize and export composers

---
+++ src/transportLayer/http/composers/index.js
@@ -0,0 +1,9 @@
+const createCartCompose = require('./create-cart-compose')
+const getCartCompose = require('./get-cart-compose')
+const addDiscountCouponCompose = require('./add-discount-coupon-compose')
+
+module.exports = {
+  createCartCompose,
+  getCartCompose,
+  addDiscountCouponCompose,
+}
