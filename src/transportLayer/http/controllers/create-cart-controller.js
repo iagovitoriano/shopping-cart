969957d3930e5c09fc60389b76e5dcaf0688d6bd
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 22:49:25 -0300

    feat: implement CreateCart controller

---
+++ src/transportLayer/http/controllers/create-cart-controller.js
@@ -0,0 +1,23 @@
+const status = require('http-status')
+
+const { createCartCompose } = require('../composers')
+
+class CreateCartController {
+  async route(httpRequest) {
+    const { customer, store } = httpRequest.body
+
+    const createCartInteractor = await createCartCompose()
+
+    const cart = await createCartInteractor.execute({
+      customer,
+      store,
+    })
+
+    return {
+      status: status.CREATED,
+      body: cart,
+    }
+  }
+}
+
+module.exports = new CreateCartController()
