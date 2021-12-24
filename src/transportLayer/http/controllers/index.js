50a35d8333b7fb1f07979f1fd94209c143f35fdf
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 23:43:51 -0300

    feat: add productControllers to entryPoint folder

--- src/transportLayer/http/controllers/index.js
+++ src/transportLayer/http/controllers/index.js
@@ -2,10 +2,16 @@ const createCartController = require('./create-cart-controller')
 const getCartController = require('./get-cart-controller')
 const clearCartController = require('./clear-cart-controller')
 const addDiscountCoupon = require('./add-discount-coupon-controller')
+const addProductController = require('./add-product-controller')
+const removeProductController = require('./remove-product-controller')
+const updateProductQuantityController = require('./update-product-quantity-controller')
 
 module.exports = {
   createCartController,
   getCartController,
   clearCartController,
   addDiscountCoupon,
+  addProductController,
+  removeProductController,
+  updateProductQuantityController,
 }
