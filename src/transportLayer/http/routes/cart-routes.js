36ddd4920474180f942f34ba8b2181e40429ab61
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 23:04:13 -0300

    feat: add clearCart route

--- src/transportLayer/http/routes/cart-routes.js
+++ src/transportLayer/http/routes/cart-routes.js
@@ -3,12 +3,14 @@ const { expressRouterAdapter } = require('../helpers')
 const {
   createCartController,
   getCartController,
+  clearCartController,
   addDiscountCoupon,
 } = require('../controllers')
 
 module.exports = (router) => {
   router.post('/cart/', expressRouterAdapter(createCartController))
   router.get('/cart/:id', expressRouterAdapter(getCartController))
+  router.patch('/cart/:id/clear', expressRouterAdapter(clearCartController))
   router.patch(
     '/cart/:id/discount-coupon',
     expressRouterAdapter(addDiscountCoupon)
