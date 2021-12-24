4e3d1b8d66c1608f976dbefc423beb0ab00525ef
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 22:50:03 -0300

    feat: centralize controllers and export in a single module

---
+++ src/transportLayer/http/controllers/index.js
@@ -0,0 +1,9 @@
+const createCartController = require('./create-cart-controller')
+const getCartController = require('./get-cart-controller')
+const addDiscountCoupon = require('./add-discount-coupon-controller')
+
+module.exports = {
+  createCartController,
+  getCartController,
+  addDiscountCoupon,
+}
