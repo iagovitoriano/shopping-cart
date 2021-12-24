2933f59de72e2f0c9eee5e641dc46a989f65f89e
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-23 23:10:55 -0300

    feat: centralize and export products interactors

--- src/transportLayer/http/composers/index.js
+++ src/transportLayer/http/composers/index.js
@@ -2,10 +2,16 @@ const createCartCompose = require('./create-cart-compose')
 const getCartCompose = require('./get-cart-compose')
 const addDiscountCouponCompose = require('./add-discount-coupon-compose')
 const clearCartCompose = require('./clear-cart-compose')
+const addProductCompose = require('./add-product-compose')
+const removeProductCompose = require('./remove-product-compose')
+const updateProductQuantityCompose = require('./update-product-quantity-compose')
 
 module.exports = {
   createCartCompose,
   getCartCompose,
   clearCartCompose,
   addDiscountCouponCompose,
+  addProductCompose,
+  removeProductCompose,
+  updateProductQuantityCompose,
 }
