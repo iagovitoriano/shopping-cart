094d8942f2b585907bdcf22da7837023fc091807
Iago Vitoriano <iago_vitoriano@icloud.com>
2021-12-26 15:39:59 -0300

    refactor: basic and coupled implementation of DTO validator

--- src/transportLayer/http/helpers/express-router-adapter.js
+++ src/transportLayer/http/helpers/express-router-adapter.js
@@ -1,8 +1,22 @@
+const { UnprocessableEntityError } = require('../errors')
+
 module.exports = (router) => async (req, res) => {
   const httpRequest = {
     body: req.body,
     params: req.params,
     query: req.query,
+  }
+
+  if (router.getDto) {
+    const dto = await router.getDto()
+
+    try {
+      await dto.validateAsync(httpRequest.body, {
+        abortEarly: false,
+      })
+    } catch (err) {
+      throw new UnprocessableEntityError('Invalid Schema')
+    }
   }
 
   const httpResponse = await router.route(httpRequest)
